9236c8752f9d905050d18b67e91b4591ecbfbd2e
srodrigo28 <rodrigoexer1@gmail.com>
2022-02-04 20:45:38 -0300

    analise cap 3

--- Cap-3/insertionSort.js
+++ Cap-3/insertionSort.js
@@ -1,18 +1,25 @@
-const livros = require('./listaLivros')
+const livros = require('./listaLivros.js')
+/**
+ * Analise
+ * https://cursos.alura.com.br/course/algoritmos-javascript-i-algoritmos-ordenacao/task/98078
+ */
+console.log(livros)
 
 function insertionSort(lista){
     for(let atual = 0; atual < lista.length; atual++){
         let analise = atual;
-
-        while(lista[analise].preco < lista[analise - 1].preco  ){
+        while( analise > 0 && lista[analise].preco < lista[analise - 1].preco  ){
             let itemAnalise = lista[analise]
             let itemAnterior = lista[analise - 1];
 
             lista[analise] = itemAnterior
-            lista[analise] = itemAnalise
+            lista[analise - 1] = itemAnalise
 
             analise--
         }
     }
     console.log(lista);
 }
+
+insertionSort(livros)
+
